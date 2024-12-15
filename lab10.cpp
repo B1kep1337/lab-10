4d336229487883d88c3f7c6eea0d9bb266306c44
B1kep <nickita.pylnov@yandex.ru>
2024-12-15 12:52:13 +0300

    Изменение лабораторной работы 10

--- lab10.cpp
+++ lab10.cpp
@@ -96,6 +96,22 @@ void getEtc(int** D, int size, int* etc) {
         etc[i] = max;
     }
 }
+//Функция создания радиуса
+int getRadius(int* etc, int size) {
+    int min = etc[0];
+    for (int i = 1; i < size; ++i) {
+        min = (etc[i] < min) ? etc[i] : min;
+    }
+    return min;
+}
+//Функция создания диаметра
+int getDiametr(int* etc, int size) {
+    int max = etc[0];
+    for (int i = 1; i < size; ++i) {
+        max = (etc[i] > max) ? etc[i] : max;
+    }
+    return max;
+}
 
 int main(int argc, char* argv[]) {
     setlocale(LC_ALL, "Rus");
@@ -113,15 +129,29 @@ int main(int argc, char* argv[]) {
             else if (strcmp(argv[i], "-o") == 0) oriented = 1;
         }
     }
-    G = createG(n);
+    else printf("Размер графа (-n <значение>), ориентированность (-o), взвешенность (-w)\n");
+    printf("n=%d w=%d o=%d\n\n", n, weighted, oriented);
+    if (weighted) G = createG(n);
+    else G = createG(n);
     if (!oriented) more(G, n);
     printG(G, n);
-
     int** D = (int**)malloc(n * sizeof(int*));
+    int* etc = (int*)malloc(n * sizeof(int));
     for (int i = 0; i < n; ++i) {
         D[i] = (int*)malloc(n * sizeof(int));
     }
     createDist(G, n, D);
+    printG(D, n);
+
+    getEtc(D, n, etc);
+    int r = getRadius(etc, n);
+    int d = getDiametr(etc, n);
+    for (int i = 0; i < n; ++i) {
+        //printf("%d ", ecc[i]);
+        if (etc[i] == d) printf("Вершина %d: переферийная\n", i);
+        if (etc[i] == r) printf("Вершина %d: центральная\n", i);
+    }
+    printf("\nРадиус : %d\nДиаметр: %d\n\n", r, d);
 
     printf("Матрица расстояний:\n");
     for (int i = 0; i < n; ++i) {
